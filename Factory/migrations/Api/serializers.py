37edce3305de8a9f799018bc5fb6432ac7cbae32
Fhiranoyama <162184595+Fhiranoyama@users.noreply.github.com>
2024-03-08 20:10:46 -0300

    FactorySerializer
    Criaçao da pasta serializer e importaçoes

---
+++ Factory/migrations/Api/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+from Factory import models
+
+class FactorySerializer(serializers.modelSerializer):
+    
--- Factory/models.py
+++ Factory/models.py
@@ -5,6 +5,6 @@ class Factory(models.model):
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     nome = models.CharField(verbose_name="nome", max_length=255)
-    data = models.CharField(verbose_name="data", max_length=30)
+    data = models.CharField(verbose_name="data", max_length=255)
     quantidade = models.datefield(auto_now_add=True)
-    calorias = models.CharField(verbose_name="calorias", max_length=30)
+    calorias = models.CharField(verbose_name="calorias", max_length=255)
