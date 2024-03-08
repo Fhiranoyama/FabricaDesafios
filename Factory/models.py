a84740b72b608d29863cfbc6f3ee56044bb5af1b
Fhiranoyama <162184595+Fhiranoyama@users.noreply.github.com>
2024-03-08 20:02:17 -0300

    Factorymodel
    criaçao dos models

--- Factory/models.py
+++ Factory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class Factory(models.model):
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    nome = models.CharField(verbose_name="nome", max_length=255)
+    data = models.CharField(verbose_name="data", max_length=30)
+    quantidade = models.datefield(auto_now_add=True)
+    calorias = models.CharField(verbose_name="calorias", max_length=30)
