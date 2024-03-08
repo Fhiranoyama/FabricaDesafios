186f31dbd4150ad38d9fdf26460bd9b65d3db088
Fhiranoyama <162184595+Fhiranoyama@users.noreply.github.com>
2024-03-08 20:13:14 -0300

    Factory views
    atualizaçao e criaçao do viewsets

--- Factory/migrations/Api/serializers.py
+++ Factory/migrations/Api/serializers.py
@@ -2,4 +2,6 @@ from rest_framework import serializers
 from Factory import models
 
 class FactorySerializer(serializers.modelSerializer):
-    
+    class Meta:
+        model = models.Factory
+        fields = '__all__'
