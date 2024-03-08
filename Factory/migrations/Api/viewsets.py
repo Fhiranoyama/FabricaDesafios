a26600b070b91f10e3f6a4149d0daebcae639878
Fhiranoyama <162184595+Fhiranoyama@users.noreply.github.com>
2024-03-08 20:21:24 -0300

    Factoryviewsets
    serializer no views

--- Factory/migrations/Api/viewsets.py
+++ Factory/migrations/Api/viewsets.py
@@ -1,2 +1,7 @@
 from rest_framework import viewsets
 from Factory.api import serializers
+from Factory import models
+
+class FactoryViewsSet(viewsets.ModelViewSet):
+    serializer_class = serializer.FactorySerializer
+    queryset = models.Factory.objects.all()
