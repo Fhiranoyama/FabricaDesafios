48270addc1a52b0df6ba85674187c5e722356603
Fhiranoyama <162184595+Fhiranoyama@users.noreply.github.com>
2024-03-08 22:40:31 -0300

    FactoryJson
    adicionando Json de nutriçao alimentar

--- Factory/Api/viewsets.py
+++ Factory/Api/viewsets.py
@@ -2,6 +2,16 @@ from rest_framework import viewsets
 from Factory.Api import serializers
 from Factory import models
 
-class FactoryViewsSet(viewsets.ModelViewSet):
+import requests
+
+
+class FactoryViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.FactorySerializer
     queryset = models.Factory.objects.all()
+
+def create(self, request):
+    nome = request.data.get('name', '')
+    site = 'https://caloriasporalimentoapi.herokuapp.com/api/calorias/?descricao={}'.format(nome)
+    req = requests.get(site)
+    json = req.json()
+    print(json)
